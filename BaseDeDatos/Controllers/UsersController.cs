1a86e4f5b9a5b53c1e15cf05968f06015668b3a4
dayan patiño <dayan24agudelo@gmail.com>
2021-07-06 15:53:13 -0500

    Agregar archivos de proyecto.

---
+++ BaseDeDatos/Controllers/UsersController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BaseDeDatos.DataAccess;
+
+namespace BaseDeDatos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            try
+            {
+                return Ok(_context.Users.ToList());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}", Name = nameof(GetUsers))]
+        public async Task<ActionResult<User>> GetUsers(int id)
+        {
+            try
+            {
+                var users = await _context.Users.FindAsync(id);
+                return Ok(users);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+     
+
+        /// <summary>
+        /// Se utiliza [FromBody para que todo vaya en el json lo que se envia]
+        /// </summary>
+        /// <param name="users"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult PostUsers([FromBody] User users)
+        {
+            try
+            {
+                _context.Users.Add(users);
+                _context.SaveChangesAsync();
+
+                return CreatedAtAction(nameof(GetUsers), new { Identity = users.Identity }, users);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUsers(int id, User users)
+        {
+            if (id != users.Identity)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(users).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUsers(int id)
+        {
+            var users = await _context.Users.FindAsync(id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(users);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UsersExists(int id)
+        {
+            return _context.Users.Any(e => e.Identity == id);
+        }
+    }
+}
---
+++ BaseDeDatos/DataAccess/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaseDeDatos.DataAccess
+{
+    [Table("USUARIO")]
+    public class User
+    {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("IDENTIFICACION")]
+        public int Identity { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Column("NOMBRE")]
+        public string FirtName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Column("APELLIDOS")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Column("CELULAR")]
+        public int Cel { get; set; }
+
+        [Required]
+        [Column("CORREO")]
+        public string Email { get; set; }
+
+        [Required]
+        [Column("DIRECCION")]
+        public string Direction { get; set; }
+
+    }
+}
